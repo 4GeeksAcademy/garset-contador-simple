0c2e54515d6c2ad4c914025321a303061c036474
Elena <pitufa247@gmail.com>
2025-05-06 17:30:46 +0000

    agrege los bonus

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -8,14 +8,28 @@ import { Digi } from "./Digi.jsx";
 const Home = () => {
 
 	const [timer, setTimer] = useState (0)
-	
+	const [active, setActive] = useState (false)
+	const [isCountDown, setIsCountDown] = useState (false)
+	const [alert, setAlert] = useState (0)
+
 	useEffect (() => {
+		if (timer == alert && alert != 0) window.alert("Tiempo cumplido")
+		if (active) {
+			setTimeout (()=>{
+				setTimer (value => value + 1)	
+			},1000)
+		}
+		if (isCountDown) {
+			setTimeout (()=>{
+				setTimer (value => value - 1)	
+			},1000)
+		}
+		
+	},[timer, active, isCountDown])
 
-		setTimeout (()=>{
-			setTimer (value => value + 1)	
-		},1000)
-
-	},[timer])
+    const startStop = () => setActive(value => !value)
+	const resetTimer = () => setTimer(value => value=0)
+	const handleChange = e =>  setTimer (value=> value= e.target.value)
 
 	return (
 		<main className="text-center">
@@ -28,6 +42,40 @@ const Home = () => {
 				<Digi number={Math.floor(timer/10)%10}/>
 				<Digi number={Math.floor(timer%10)}/>
 			</section>
+			<section className="container text-center my-5">
+				<h1>Control del contador</h1>
+					<div>
+						<button disabled={active} onClick={startStop} className="mx-2 btn btn-success">Inicio</button>
+						<button disabled={!active} onClick={startStop} className="mx-2 btn btn-danger">Detener</button>
+						<button onClick={resetTimer} className="mx-2 btn btn-warning">Reiniciar</button>
+					</div>
+			</section>
+			<section className="container text-center my-5">
+				<h1>Cuenta Regresiva</h1>
+				<form className="form-control" onSubmit={e=>(e.preventDefault())}>
+				<label className="form-text">
+					Cantidad para empezar
+				<input className="form-control" type="number" value={timer} onChange={e=> handleChange(e)}/>	
+				</label>
+					<div>
+						<input disabled={isCountDown} onClick={()=>setIsCountDown (value => !value)} className="m-2 btn btn-success" type="submit" value={"Enviar"}/>	
+						<input disabled={!isCountDown} onClick={()=>setIsCountDown (value => !value)} className="m-2 btn btn-danger" type="submit" value={"Detener"}/>	
+					</div>
+				</form> 
+			</section>
+			<section className="container text-center my-5">
+				<h1>Crear Alerta</h1>
+				<form className="form-control" onSubmit={e => e.preventDefault()}>
+				<label className="form-text">
+					Alertar a:
+				<input className="form-control" type="number" onChange={e => setAlert (value => value = e.target.value)}/>	
+				</label>
+					<div>
+					<input onClick={()=> window.alert("Alerta creada")} className="m-2 btn btn-success" type="submit" value={"Crear"}/>	
+					</div>
+				</form>
+			</section>
+
 		</main>
 	);
 };
